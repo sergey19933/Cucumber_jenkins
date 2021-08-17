ae8d893aaae0ddeafd8b7e16673c356c679d8f6b
Sergey Churikov <spell1993@yandex.ru>
2021-08-17 14:33:13 +0300

    jenkinsAddfile

---
+++ Jenkinsfile_scripted.groovy
@@ -0,0 +1,31 @@
+def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
+
+node {
+    stage('Checkout SCM') {
+        checkout(
+                [$class: 'GitSCM',
+                 branches: [[name: "refs/heads/${BRANCH}"]],
+                 userRemoteConfigs: [[url: 'https://github.com/sergey19933/Cucumber_jenkins.git']]]
+        )
+    }
+    stage('Build') {
+        sh "${mvn} clean compile"
+    }
+    stage('Run Tests') {
+        try {
+            sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
+        }
+        catch (Exception e) {
+            echo "Test run was broken"
+            throw e
+        }
+        finally {
+            stage('Allure Report Generation') {
+                allure includeProperties: false,
+                        jdk: '',
+                        results: [[path: 'target/reports/allure-results']]
+            }
+            cleanWs notFailBuild: true
+        }
+    }
+}
